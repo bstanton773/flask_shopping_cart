7428322f11ee15f9d29ba6e3e3ef1444b72655b6
Brian <bstanton773@gmail.com>
2021-10-18 14:09:37 -0500

    Submitting the form

--- app/routes.py
+++ app/routes.py
@@ -8,7 +8,11 @@ def index():
     return render_template('index.html', name='Brian')
 
 
-@app.route('/register')
+@app.route('/register', methods=["GET", "POST"])
 def register():
     register_form = RegistrationForm()
+    if register_form.validate_on_submit():
+        username = register_form.username.data
+        password = register_form.password.data
+        print(username, password)
     return render_template('register.html', form=register_form)
